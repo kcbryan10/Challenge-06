12015e013ee00ef97ca007fd2b9275c7955d397d
kcbryan <kcbryan10@gmail.com>
2021-10-27 13:37:41 -0500

    added const for ids in HTML

---
+++ assets/JS/script.js
@@ -0,0 +1,14 @@
+function initPage(){
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    const APIKey = "c9a9ed03a355403f4cb9a36e931c0b4a";
+}
+    
